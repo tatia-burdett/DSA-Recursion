70207bcf4e696326285713dcdb0eb232b2422139
Tatia burdett <tatiaburdett@gmail.com>
2021-04-15 13:57:38 -0700

    change function name

--- drills/nth-triangle.js
+++ drills/nth-triangle.js
@@ -1,12 +1,12 @@
 // The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n. This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
 
-const triangular = function(n) {
+const nth_triangular = function(n) {
   if (n === 1) {
     return 1
   }
-  return n + triangular(n - 1)
+  return n + nth_triangular(n - 1)
 }
 
 let n = 7
 
-console.log(triangular(n))
+console.log(nth_triangular(n))
