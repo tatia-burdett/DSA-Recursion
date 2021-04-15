52c697d875abe53e3081339495a22d18ed8f84c8
Tatia burdett <tatiaburdett@gmail.com>
2021-04-15 11:33:57 -0700

    write powerCalc drill, add folder

--- drills/powerCalc.js
+++ drills/powerCalc.js
@@ -3,8 +3,14 @@
 // use only exponents greater than or equal to 0
 
 const powerCalculator = function(base, exponent) {
-  if (exponent < 0) {
-    return `Exponent should be >= 0`
+  if (exponent <= -1) {
+    'exponent should be >= 0'
   }
-  
+  if(exponent === 0) {
+    return 1
+  }
+  console.log(base, exponent)
+  return base * powerCalculator(base, (exponent - 1))
 }
+
+console.log(powerCalculator(10, 2))
